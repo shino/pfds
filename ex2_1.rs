86119247fca1c8f8c6b28aa4cd5e1f4fa8cb6c78
Shunichi Shinohara <shino.shun@gmail.com>
2017-07-03 18:36:11 +0900

    More struggle with linked list

--- ex2_1.rs
+++ ex2_1.rs
@@ -1,14 +1,36 @@
 use std::rc::Rc;
 use std::fmt;
 
+/// PFDS 演習問題2.1
+/// > suffixes [1,2,3,4] = [[1,2,3,4], [2,3,4], [3,4], [4], []]
+///
+/// Check
+/// - [ ] correctness
+/// - [ ] O(n)
+
 fn main() {
-    let vec = vec![1,2,3];
+    let vec = vec![1,2,3, 4];
     let list = List3::new_list(&vec);
     println!("list: {}", list);
 
     let suffixes = suffixes(list);
     println!("suffixes: {}", suffixes);
     println!("suffixes: {:?}", suffixes);
+
+    let list2 = List3::new_list(&vec);
+    let sum = List3::foldl(0, |sum, x| {*sum + x}, &list2);
+    let sum_rec = List3::foldl_rec(0, |sum, x| *sum + x, &list2);
+    println!("sum: {}", sum);
+    println!("sum_rec: {}", sum_rec);
+
+    let strs = vec!["abc".to_string(), "def".to_string(), "xyz".to_string()];
+    println!("concat: {}",
+             List3::foldl("".to_string(), |res, str| format!("{}{}", res, str),
+                          &List3::new_list(&strs)));
+    println!("concat_rec: {}",
+             List3::foldl_rec("".to_string(), |res, str| format!("{}{}", res, str),
+                              &List3::new_list(&strs)));
+
 }
 
 fn suffixes<T>(list: List3<T>) -> List3<List3<T>>{
@@ -32,6 +54,40 @@ impl<T> List3<T> {
         })
     }
 
+    fn foldl<U, F>(acc0: U, f: F, list: &List3<T>) -> U
+        where F: Fn(&T, U) -> U {
+        let mut acc = acc0;
+        let mut sublist = list;
+        loop {
+            match sublist {
+                &List3(ref rc_cons) =>
+                    // rc_cons : &std::rc::Rc<List3Segment<T>>
+                    match **rc_cons {
+                        List3Segment::Nil =>
+                            return acc,
+                        List3Segment::Cons(ref hd, ref tl) => {
+                            acc = f(hd, acc);
+                            sublist = tl;
+                        }
+                    }
+            }
+        }
+    }
+
+    fn foldl_rec<U, F>(acc: U, f: F, list: &List3<T>) -> U
+        where F: Fn(&T, U) -> U {
+        match list {
+            &List3(ref rc_cons) => {
+                match **rc_cons {
+                    List3Segment::Cons(ref hd, ref tl) =>
+                        List3::foldl_rec(f(hd, acc), f, tl),
+                    List3Segment::Nil =>
+                        acc
+                }
+            }
+        }
+    }
+
     fn cons(x: T, list: List3<T>) -> List3<T> {
         List3(Rc::new(List3Segment::Cons(x, list)))
     }
@@ -44,6 +100,19 @@ impl<T> List3<T> {
 
 impl<T> fmt::Display for List3<T> where T : fmt::Display {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // TODO: Want to use foldl and closure, but does not work.
+        // error[E0281]: type mismatch: the type `[closure@ex2_1.rs:104:29: 104:71 f:_]`
+        // implements the trait `std::ops::Fn<(_, _)>`, but the trait
+        // `for<'r> std::ops::Fn<(&'r T, ())>` is required (expected concrete lifetime,
+        // found bound lifetime parameter )
+        //
+        // let _ = write!(f, "["]);
+        // {
+        //     let write_one = |x, acc| { let _ = write!(f, "{}, ", x); };
+        //     self.foldl((), write_one);
+        // }
+        // write!(f, "]")
+
         match self {
             &List3(ref cons_rc) =>
                 match **cons_rc {
